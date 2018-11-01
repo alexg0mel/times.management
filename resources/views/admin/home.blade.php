571f62517fdf06f1929466fca05e4774e5f78dfe
alexgomel <alexgomel@tut.by>
2018-11-01 12:46:19 +0300

    navigate for demo

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
     @include ('admin._nav', ['page' => ''])
+
+    <p> Админка, для демо, открыта для всех. </p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,13 @@
                     <div id="app">
                         <step token="{{ $token }}"></step>
                     </div>
+                    <div class="links container pt-4">
+                        <p class="btn btn-secondary"> ссылки для демо</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item list-group-item-success"><a href=" {{ route('admin.home') }}">admin zone</a> Здесь настраиваются проекты/задачи и таймлайны</li>
+                            <li class="list-group-item list-group-item-success"><a href="{{ route('cabinet.task.active') }}">my tasks</a> Личная зона - привязка проектов к пользователю</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
